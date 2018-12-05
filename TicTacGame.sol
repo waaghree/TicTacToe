ae1c7787742509ab0b7b8ded495d0621b0a08d7c
waaghree <waaghree@hotmail.com>
2018-12-05 18:27:57 +0500

    2nd commit

---
+++ TicTacGame.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+contract TicTacGame {
+    
+   enum CrossOrEx {
+        Ex, Oh
+    }
+   
+   function checkForWinner() public {
+       
+   }
+   
+   function restartGame() public constant returns (bool) {
+       
+   }
+
+   function makeMove()
+    {
+
+    }
+
+    function canPlayHere()
+    {
+
+    }
+
+    function makeMove()
+    {
+
+    }
+    
+}
